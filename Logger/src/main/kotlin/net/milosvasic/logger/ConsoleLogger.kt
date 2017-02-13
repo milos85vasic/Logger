968470b0aca20ef08449846a94818c3dc22c1154
Milos Vasic <office@milosvasic.net>
2017-02-13 10:43:46 +0100

    Logger: Working on filesystem log implementation.

--- Logger/src/main/kotlin/net/milosvasic/logger/ConsoleLogger.kt
+++ Logger/src/main/kotlin/net/milosvasic/logger/ConsoleLogger.kt
@@ -1,16 +1,10 @@
 package net.milosvasic.logger
 
-
-import java.text.SimpleDateFormat
-import java.util.*
 import kotlin.reflect.KClass
 
-class ConsoleLogger : Logger {
+class ConsoleLogger : CommonLogger() {
 
-    private val space = 12
-    private val tagLength = 30
     private val output = System.out
-    private val dateFormat = SimpleDateFormat("HH:mm:ss:S")
     private val loggingPattern = "%s[ %s ][ %s ][ %s ] %s%s"
 
     override fun v(tag: KClass<*>, message: String) {
@@ -116,46 +110,6 @@ class ConsoleLogger : Logger {
                         LogColor.ANSI_RESET
                 )
         )
-    }
-
-    private fun getLogLevel(level: LOG_LEVEL): String = getText(level.name, space)
-
-    private fun getDatetime(): String = getText(dateFormat.format(Date()), space - 1)
-
-    private fun getTag(tag: KClass<*>): String {
-        var tagValue = "${tag.simpleName}"
-        if (tagValue.length >= tagLength) {
-            tagValue = "${tagValue.substring(0, tagLength - 2)}..."
-        }
-        return getText(tagValue, tagLength)
-    }
-
-    private fun getMessage(message: String, tag: String): String {
-        if (message.contains("\n")) {
-            val builder = StringBuilder()
-            val items = message.split("\n")
-            items.forEachIndexed { i, item ->
-                if (i > 0) {
-                    builder.append("\n")
-                    val spaces = (space * 3) + tag.length + 1
-                    for (x in 0..spaces) {
-                        builder.append(" ")
-                    }
-                }
-                builder.append(item)
-            }
-            return builder.toString()
-        }
-        return message
-    }
-
-    private fun getText(text: String, length: Int): String {
-        val builder = StringBuilder()
-        builder.append(text)
-        for (x in 0..(length - text.length)) {
-            builder.append(" ")
-        }
-        return builder.toString()
     }
 
     private object LogColor {
--- Logger/src/main/kotlin/net/milosvasic/logger/FilesystemLogger.kt
+++ Logger/src/main/kotlin/net/milosvasic/logger/FilesystemLogger.kt
@@ -1,36 +1,122 @@
 package net.milosvasic.logger
 
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.reflect.KClass
 
-class FilesystemLogger(val root: File = File(System.getProperty("user.dir"))) : Logger {
+class FilesystemLogger(val root: File = File(System.getProperty("user.dir"))) : CommonLogger() {
+
+    var extension = "log"
+    var structured = true
+    private val loggingPattern = "[ %s ][ %s ][ %s ] %s"
+    private val filenameDateFormat = SimpleDateFormat("Y_M_d")
 
     override fun v(tag: KClass<*>, message: String) {
-        
+        val tagValue = getTag(tag)
+        getDestination().appendText(
+                String.format(
+                        loggingPattern,
+                        getLogLevel(LOG_LEVEL.VERBOSE),
+                        getDatetime(),
+                        tagValue,
+                        getMessage(message, tagValue)
+                )
+        )
+        getDestination().appendText("\n")
     }
 
     override fun d(tag: KClass<*>, message: String) {
-
+        val tagValue = getTag(tag)
+        getDestination().appendText(
+                String.format(
+                        loggingPattern,
+                        getLogLevel(LOG_LEVEL.DEBUG),
+                        getDatetime(),
+                        tagValue,
+                        getMessage(message, tagValue)
+                )
+        )
+        getDestination().appendText("\n")
     }
 
     override fun c(tag: KClass<*>, message: String) {
-
+        val tagValue = getTag(tag)
+        getDestination().appendText(
+                String.format(
+                        loggingPattern,
+                        getLogLevel(LOG_LEVEL.CONFIRMATION),
+                        getDatetime(),
+                        tagValue,
+                        getMessage(message, tagValue)
+                )
+        )
+        getDestination().appendText("\n")
     }
 
     override fun n(tag: KClass<*>, message: String) {
-
+        val tagValue = getTag(tag)
+        getDestination().appendText(
+                String.format(
+                        loggingPattern,
+                        getLogLevel(LOG_LEVEL.NOTIFICATION),
+                        getDatetime(),
+                        tagValue,
+                        getMessage(message, tagValue)
+                )
+        )
+        getDestination().appendText("\n")
     }
 
     override fun i(tag: KClass<*>, message: String) {
-
+        val tagValue = getTag(tag)
+        getDestination().appendText(
+                String.format(
+                        loggingPattern,
+                        getLogLevel(LOG_LEVEL.INFORMATION),
+                        getDatetime(),
+                        tagValue,
+                        getMessage(message, tagValue)
+                )
+        )
+        getDestination().appendText("\n")
     }
 
     override fun w(tag: KClass<*>, message: String) {
-
+        val tagValue = getTag(tag)
+        getDestination().appendText(
+                String.format(
+                        loggingPattern,
+                        getLogLevel(LOG_LEVEL.WARNING),
+                        getDatetime(),
+                        tagValue,
+                        getMessage(message, tagValue)
+                )
+        )
+        getDestination().appendText("\n")
     }
 
     override fun e(tag: KClass<*>, message: String) {
+        val tagValue = getTag(tag)
+        getDestination().appendText(
+                String.format(
+                        loggingPattern,
+                        getLogLevel(LOG_LEVEL.ERROR),
+                        getDatetime(),
+                        tagValue,
+                        getMessage(message, tagValue)
+                )
+        )
+        getDestination().appendText("\n")
+    }
 
+    private fun getDestination(): File {
+        var home = root
+        val filename = "${filenameDateFormat.format(Date())}.$extension"
+        if (structured) {
+            // TODO: To be implemented.
+        }
+        return File(home.absolutePath, filename)
     }
 
 }
