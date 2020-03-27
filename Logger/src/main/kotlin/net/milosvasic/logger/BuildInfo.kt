ebcc67118a5ff815126d8115e152a20d3dd4a277
Milos Vasic <milos85vasic@gmail.com>
2020-03-27 18:06:22 +0100

    Application name.

--- Logger/src/main/kotlin/net/milosvasic/logger/BuildInfo.kt
+++ Logger/src/main/kotlin/net/milosvasic/logger/BuildInfo.kt
@@ -2,7 +2,7 @@ package net.milosvasic.logger
 
 object BuildInfo {
 
-    const val name = "Logger"
+    const val NAME = "Logger"
     const val VERSION = "1.3.1"
     const val VERSION_CODE = (130 * 1000) + 2
 }
