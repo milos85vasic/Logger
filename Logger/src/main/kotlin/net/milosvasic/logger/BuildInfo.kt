94349b49b2361ca10f3ec6fbcdc37d3ad682ee44
Milos Vasic <milos85vasic@gmail.com>
2020-06-10 16:30:28 +0200

    Version increased to 1.3.7.

--- Logger/src/main/kotlin/net/milosvasic/logger/BuildInfo.kt
+++ Logger/src/main/kotlin/net/milosvasic/logger/BuildInfo.kt
@@ -3,6 +3,6 @@ package net.milosvasic.logger
 object BuildInfo {
 
     const val NAME = "Logger"
-    const val VERSION = "1.3.6"
-    const val VERSION_CODE = (130 * 1000) + 7
+    const val VERSION = "1.3.7"
+    const val VERSION_CODE = (130 * 1000) + 8
 }
