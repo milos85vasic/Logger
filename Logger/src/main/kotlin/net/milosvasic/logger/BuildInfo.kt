e1c084fe1999073850c72668742aa64c44efef7b
Milos Vasic <milos85vasic@gmail.com>
2020-06-10 15:11:33 +0200

    Version 1.3.5.

--- Logger/src/main/kotlin/net/milosvasic/logger/BuildInfo.kt
+++ Logger/src/main/kotlin/net/milosvasic/logger/BuildInfo.kt
@@ -3,6 +3,6 @@ package net.milosvasic.logger
 object BuildInfo {
 
     const val NAME = "Logger"
-    const val VERSION = "1.3.4"
-    const val VERSION_CODE = (130 * 1000) + 5
+    const val VERSION = "1.3.5"
+    const val VERSION_CODE = (130 * 1000) + 6
 }
